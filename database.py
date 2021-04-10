2a496bd1d27a147842333e2568fe5a47124b1f94
Jordan Bach <jordanbach@gmail.com>
2021-04-09 20:06:23 -0500

    make database connection

--- database.py
+++ database.py
@@ -1,4 +1,11 @@
+import os
 import sqlalchemy
 
+DB_URL = os.getenv("DB_URL", "mysql+pymysql://localhost")
+
 def sql_engine():
-    return sqlalchemy.create_engine("mysql+pymysql://localhost", future=True)
+    return sqlalchemy.create_engine(DB_URL, future=True)
+
+def sql_connection():
+    engine = sql_engine()
+    return engine.connect()
--- test/database_test.py
+++ test/database_test.py
@@ -1,7 +1,13 @@
 import database
+import sqlalchemy
 
 def test_engine():
     engine = database.sql_engine()
     assert engine.name == 'mysql'
     assert engine.driver == 'pymysql'
     assert 'future' in engine.__module__
+
+def test_connection():
+    with database.sql_connection() as conn:
+        result = conn.execute(sqlalchemy.text("select 'hello world'"))
+        assert 'hello world' == result.first()[0]
